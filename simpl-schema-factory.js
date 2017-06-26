980adf4cb98588a3a0faace9c9c07db718b92c20
jankapunkt <jkuester@uni-bremen.de>
2017-06-26 18:09:07 +0200

    initial setup

---
+++ simpl-schema-factory.js
@@ -0,0 +1,116 @@
+// CHECK NPM VERSIONS FIRST
+
+import {checkNpmVersions} from 'meteor/tmeasday:check-npm-versions';
+import {Tracker} from 'meteor/tracker';
+import {check, Match} from 'meteor/check';
+//import {Roles} from 'meteor/alanning:roles';
+
+checkNpmVersions({"simpl-schema": '0.x.x'}, 'jkuester:simpl-schema-factory');
+
+const SimpleSchema = require('simpl-schema').default;
+
+
+export const SimpleSchemaFactory = {
+
+
+	//////////////////////////////////////////////////////////////////////////////
+
+	docId(){
+		return this._useTracker
+			? new SimpleSchema({_id: {type: String}}, {tracker: Tracker})
+			: new SimpleSchema({_id: {type: String}});
+	},
+
+	clear() {
+		this._defaultPublicFields = null;
+		this._defaultSchema = null;
+		this._useTracker = false;
+	},
+
+	custom(schema) {
+		return new SimpleSchema(schema);
+	},
+
+	//////////////////////////////////////////////////////////////////////////////
+
+	_defaultSchema: {},
+
+	defaultSchema(schemaObj = null) {
+		if (schemaObj && typeof schemaObj === 'object')
+			this._defaultSchema = schemaObj;
+		console.log(this._defaultSchema, SimpleSchema);
+		return this._useTracker
+			? new SimpleSchema(this._defaultSchema, {tracker: Tracker})
+			: new SimpleSchema(this._defaultSchema);
+	},
+
+	defaultSchemaWith(customSchema){
+		const customSchemaDef = this._useTracker
+			? new SimpleSchema(customSchema, {tracker: Tracker})
+			: new SimpleSchema(customSchema)
+		customSchemaDef.extend(this.defaultSchema());
+		return customSchemaDef;
+	},
+
+	//////////////////////////////////////////////////////////////////////////////
+
+	_defaultPublicFields: {},
+
+	defaultPublicFields(argsObj = null){
+		if (argsObj) {
+			this._defaultPublicFields = argsObj;
+		}
+		return this._defaultPublicFields
+	},
+
+	defaultPublicFieldsWith(customFields) {
+		return Object.assign({}, this.defaultPublicFields(), customFields);
+	},
+
+
+	//////////////////////////////////////////////////////////////////////////////
+
+	updateSingleValue(propertyName, propertyType, optional = false) {
+		const updateScheme = {};
+		const updateSchemeValues = {
+			type: propertyType,
+			optional: optional
+		};
+		updateScheme[propertyName] = updateSchemeValues;
+		return new SimpleSchema(updateScheme);
+	},
+
+	//////////////////////////////////////////////////////////////////////////////
+
+	validateDoc(doc, schema, clean = false) {
+		const vc = schema.newContext();
+		vc.validate(doc);
+		return vc.isValid();
+	},
+
+	validate(args) {
+		check(args, {
+			userId: String,
+			collection: Mongo.Collection,
+			doc: Object
+		})
+
+		//validate doc by Schema
+		//return false if failed
+		const vc = args.collection.schema.newContext();
+		vc.validate(args.doc);
+		return vc.isValid();
+	},
+
+
+	useWithAutoForm() {
+		SimpleSchema.extendOptions(['autoform']);
+	},
+
+	_useTracker: false,
+
+	useWithTracker(value) {
+		this._useTracker = value;
+	},
+
+}
---
+++ tests/simpl-schema-factory.tests.js
@@ -0,0 +1,131 @@
+/* eslint-env mocha */
+import {chai, assert} from 'meteor/practicalmeteor:chai';
+import {Random} from 'meteor/random';
+
+import {SimpleSchemaFactory} from 'meteor/jkuester:simpl-schema-factory';
+
+
+const isDefined = function(value, type = null, optionalMessage=null) {
+	assert.isDefined(value);
+	assert.isNotNull(value);
+	if (type) assert.typeOf(value, type, optionalMessage);
+};
+
+const validateBySchema = function (schema, validObj, invalidObj) {
+	isDefined(schema, 'object', "schema obj");
+	isDefined(validObj, 'object', "validObj");
+	isDefined(invalidObj, 'object', "invalidObj");
+
+	const vc = schema.newContext();
+	vc.clean(validObj);
+	vc.validate(validObj);
+	const isValid = vc.isValid();
+	assert.equal(isValid, true, "should be valid");
+
+	vc.validate(invalidObj);
+	const isNotValid = vc.isValid();
+	assert.equal(isNotValid, false, "should be invalid");
+};
+
+const getDefaultSchema = function() {
+	return {
+		title: String,
+		code:String,
+		description: {type:String, optional: true},
+		createdBy: String,
+		createdAt: Number
+	};
+};
+
+const getDefaultPropsWith = function(customProps = {}) {
+	return Object.assign({}, {
+		title: "title title",
+		code: Random.id(5),
+		description: "aljkd dqwpd ndadpajd nadapdjn asdas dpajüi pk jiüoj ns",
+		createdBy: Random.id(),
+		createdAt: new Date().getTime()
+	}, customProps);
+};
+
+describe("Import", function () {
+	it("is imported correctly", function () {
+		isDefined(SimpleSchemaFactory);
+	})
+})
+
+describe("SimpleSchemaFactory", function() {
+
+	beforeEach(function(){
+		SimpleSchemaFactory.defaultSchema(getDefaultSchema());
+		SimpleSchemaFactory.defaultPublicFields({
+			title: 1,
+			code:1,
+			description:1,
+		});
+	});
+	
+	afterEach(function () {
+		SimpleSchemaFactory.clear();
+	});
+	
+	it("creates defaultSchema", function() {
+		const defaultSchema = SimpleSchemaFactory.defaultSchema();
+		validateBySchema(defaultSchema, getDefaultPropsWith(), {});
+	});
+
+	it("creates docId", function() {
+		const docIdSchema = SimpleSchemaFactory.docId();
+		validateBySchema(docIdSchema, {
+			_id: Random.id(17)
+		}, {});
+	});
+
+	it("creates defaultSchemaWith", function() {
+		const customSchema = SimpleSchemaFactory.defaultSchemaWith({custom: {type: String}}, true);
+		validateBySchema(customSchema,
+			getDefaultPropsWith({custom: Random.id(17)}),
+			getDefaultPropsWith({_id: Random.id(17)}));
+	});
+
+	it("creates custom", function() {
+		const customSchema = SimpleSchemaFactory.custom({
+			some: {type: String},
+			other: {type: Number}
+		});
+		validateBySchema(customSchema, {
+			some: "test",
+			other: 10000,
+		}, {
+			some: "test",
+			other: "10000",
+		})
+	});
+
+	it("creates updateSingleValue", function() {
+		const updateSingleValue = SimpleSchemaFactory.updateSingleValue("type", String, false);
+		validateBySchema(updateSingleValue, {
+			type: "some type"
+		}, {});
+	});
+
+	it("creates defaultPublicFields", function() {
+		const defaultPublicFields = SimpleSchemaFactory.defaultPublicFields();
+		assert.deepEqual(defaultPublicFields, {
+			title: 1,
+			code: 1,
+			description: 1,
+		});
+	});
+
+	it("creates defaultPublicFieldsWith", function() {
+		const defaultPublicFields = SimpleSchemaFactory.defaultPublicFieldsWith({
+			custom: 1
+		});
+		assert.deepEqual(defaultPublicFields, {
+			title: 1,
+			code: 1,
+			description: 1,
+			custom: 1,
+		});
+	});
+})
