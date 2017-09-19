4236db90f2a73f686eab4795dc0031c15a51e626
jankapunkt <jkuester@uni-bremen.de>
2017-09-19 13:29:02 +0200

    version 0.1.1

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'jkuester:simpl-schema-factory',
-  version: '0.1.0',
+  version: '0.1.1',
   // Brief, one-line summary of the package.
   summary: 'Factory for your SimpleSchema instances, using npm simpl-schema package.',
   // URL to the Git repository containing the source code for this package.
